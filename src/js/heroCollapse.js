e1b14e72ffa4d46a910ec5b9f7a5f0b707dbc0c7
Patrick Oliveira <56600039+POliveira29@users.noreply.github.com>
2021-04-19 11:29:30 -0300

    Update heroCollapse.js

--- src/js/heroCollapse.js
+++ src/js/heroCollapse.js
@@ -1,13 +1,3 @@
-/*$(document).ready(function () {
-  $(".hero__menu").click(function () {
-    if ($(".hero__nav").hasClass("hero__nav--active")) {
-      $(".hero__nav").removeClass("hero__nav--active");
-    } else {
-      $(".hero__nav").addClass("hero__nav--active");
-    }
-  });
-});*/
-
 var menu = document.querySelector(".hero__menu");
 menu.addEventListener("click", function () {
   document.getElementById("btn-menu").classList.toggle("hero__nav--active");
